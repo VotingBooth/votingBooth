868ee207f55de969705c580360ed3254d1d37ba0
Nick Gourlay <nick@gourlay.me>
2022-08-22 17:45:28 -0400

    initial commit of chart.js for pollResults

--- src/PollResults.js
+++ src/PollResults.js
@@ -1,13 +1,69 @@
 import { useParams } from 'react-router-dom'
+import 'chart.js/auto';
+import { Chart } from 'react-chartjs-2';
+import { useState } from 'react'
 
 function PollResults() {
+    const [chartSelection, setChartSelection] = useState('bar')
 
     const { pollID } = useParams();
 
+    const testData = [156, 243]
+
+    const options = {
+        elements: {
+            bar: {
+                borderWidth: 2,
+            },
+        },
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: 'Chart.js Horizontal Bar Chart',
+            },
+        },
+    };
+
+    const labels = ['no', 'yes']
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: 'TestData',
+                data: testData,
+                borderColor: ['rgb(255, 99, 132, 0.5)', 'blue'],
+                backgroundColor: ['rgb(255, 99, 132, 0.5)', 'blue']
+            }
+        ],
+    };
+
+    const handleChange = (e) => {
+        setChartSelection(e.target.value)
+    }
     return (
         <>
             <div>{pollID}</div>
             <p>Results</p>
+            <form>
+                <legend>Select a chart type:</legend>
+                <div>
+                    <label htmlFor="doughnut">Doughnut</label>
+                    <input type="radio" id="doughnut" name="doughnut" value="doughnut" onChange={handleChange} checked={chartSelection === 'doughnut'} />
+                </div>
+                <div>
+                    <label htmlFor="bar">Bar</label>
+                    <input type="radio" id="bar" name="bar" value="bar" onChange={handleChange} checked={chartSelection === 'bar'} />
+                </div>
+            </form>
+            <div>
+                <Chart
+                    type={chartSelection}
+                    options={options}
+                    data={data}
+                />
+            </div>
         </>
     )
 }
