a0406d1fc6aafb8b1cd191bb71c4817d75f17fb6
CRobock <crobock07@gmail.com>
2022-08-22 15:23:41 -0400

    updated PollCreate component to store user question in firebase with unique ID

--- src/PollCreate.js
+++ src/PollCreate.js
@@ -1,6 +1,47 @@
+
+import {firebase} from './firebase';
+import { getDatabase, ref, push, update } from 'firebase/database';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+
 function PollCreate() {
+    const [question, setQuestion] = useState('');
+    const navigate = useNavigate();
+
+    const handleChange = (e) => {
+        setQuestion(e.target.value)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const database = getDatabase(firebase)
+        const dbRef = ref(database)
+        const newKey = push(dbRef).key;
+        const postData = {
+            question: question
+        }
+        const updates = {};
+        updates[ newKey + '/'] = postData
+        update(dbRef, updates);
+
+        navigate(`/poll/${newKey}`);
+
+    }
+
+
     return (
-        <div>PollCreate</div>
+
+        <div>
+            <h2>Enter your poll question below</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor='userInput'>Please enter your question:</label>
+                <input maxlength='140' type="text" id='userInput' onChange={handleChange}/>
+                <button>
+                    <p>Submit Question</p>
+                </button>
+            </form>
+        </div>
     )
 }
 
