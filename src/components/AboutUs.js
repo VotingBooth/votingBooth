b21d06c44a784ddc39eac9dd30b282210a4598bc
CRobock <crobock07@gmail.com>
2022-08-26 14:40:05 -0400

    started AboutUs component and began adding fontAwesome to app

---
+++ src/components/AboutUs.js
@@ -0,0 +1,24 @@
+import NavBar from "./NavBar";
+import { Link } from "react-router-dom";
+
+const AboutUs = () => {
+    return (
+        <>
+            <NavBar />
+            <section className="aboutUs">
+                <div className="wrapper flexContainer">
+                    <div className="imgContainer">
+                        <img src="http://placekitten.com/300/300" alt="headshot of creators"/>
+                        <div className="socialContainer">
+                            
+                        </div>
+                    </div>
+
+                </div>
+                
+
+                
+            </section>
+        </>
+    )
+}
