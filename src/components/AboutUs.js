b57bb85b84ef1c11bcc94f9d327825df7931a2e9
CRobock <crobock07@gmail.com>
2022-08-30 12:03:44 -0400

    completed AboutUs media queries

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,12 +1,19 @@
 import { FaGithubSquare, FaLinkedin, FaFolderOpen } from 'react-icons/fa';
 import '.././styling/AboutUs.scss'
+import Connor from '.././assets/Connor.jpg'
+import Kaitlyn from '.././assets/Kaitlyn_Resized.jpg'
+import Paridhi from '.././assets/Paridhi.jpg'
+import Nick from '.././assets/Nick.JPG'
 
 function AboutUs () {
     return (
         <section className="aboutUs">
-            <div className="wrapper flexContainer">
+            <div className="flexContainer">
+                <h2 className='aboutTitle'>About Us</h2>
+                <p>Hello, and welcome to our anonymous voting booth! Our clients tasked us with creating a fully accesible and responsive website that would allow users to create and share anonymous polls. Please take the time to create and share a poll!</p>
                 <div className="imgContainer">
-                    <img src="http://placekitten.com/300/300" alt="headshot of creators"/>
+                    <h3 className='name'>Connor Robock</h3>
+                    <img src={Connor} alt="headshot of creators"/>
                     <div className="socialContainer">
                         <a target="_blank" rel="noopener noreferrer" href="https://github.com/ConnorRobock" >
                             <FaGithubSquare />
@@ -20,7 +27,8 @@ function AboutUs () {
                     </div>
                 </div>
                 <div className="imgContainer">
-                    <img src="http://placekitten.com/300/300" alt="headshot of creators"/>
+                    <h3 className='name'>Kaitlyn Wickson</h3>
+                    <img src={Kaitlyn} alt="headshot of creators"/>
                     <div className="socialContainer">
                         <a target="_blank" rel="noopener noreferrer" href="https://github.com/kaitlynw88">
                             <FaGithubSquare />
@@ -34,7 +42,8 @@ function AboutUs () {
                     </div>
                 </div>
                 <div className="imgContainer">
-                    <img src="http://placekitten.com/300/300" alt="headshot of creators"/>
+                    <h3 className='name'>Nick Gourlay</h3>
+                    <img src={Nick} alt="headshot of creators"/>
                     <div className="socialContainer">
                         <a target="_blank" rel="noopener noreferrer" href="https://github.com/NicholG90">
                             <FaGithubSquare />
@@ -48,15 +57,16 @@ function AboutUs () {
                     </div>
                 </div>
                 <div className="imgContainer">
-                    <img src="http://placekitten.com/300/300" alt="headshot of creators"/>
+                    <h3 className='name'>Paridhi Shah</h3>
+                    <img src={Paridhi} alt="headshot of creators"/>
                     <div className="socialContainer">
-                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/ConnorRobock">
+                        <a target="_blank" rel="noopener noreferrer" href="https://github.com/paridhishah96">
                             <FaGithubSquare />
                         </a>
-                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/connor-robock-00671968/">
+                        <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/paridhishahcodes/">
                             <FaLinkedin />
                         </a>
-                        <a target="_blank" rel="noopener noreferrer" href="https://www.connorrobock.com/">
+                        <a target="_blank" rel="noopener noreferrer" href="https://paridhishah.dev/">
                             <FaFolderOpen />
                         </a>
                     </div>
